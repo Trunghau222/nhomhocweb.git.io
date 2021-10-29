0846d0ee59cb9219dc5c2514cb1608f377836fbe
Trunghau <nguyentrunghau2222000@gmail.com>
2021-10-29 21:56:06 +0700

    version baitap

--- mydb.php
+++ mydb.php
@@ -1,28 +1,31 @@
 <?php 
- // kết nối
- $servername = "localhost";// địa chỉ 
- $database = "test";
- $username = "root";//
- $password = "";
- // Create connection
- $conn = mysqli_connect($servername, $username, $password, $database);
- // Check connection
- if (!$conn) {
-     die("Connection failed: " . mysqli_connect_error());// hàm die là hàm chương trình 
- }
+echo "toan";
+//  // kết nối
+//  $servername = "localhost";// địa chỉ 
+//  $database = "test1231321";
+//  $username = "root";//
+//  $password = "";
+//  // Create connection
+//  $conn = mysqli_connect($servername, $username, $password, $database);
+//  // Check connection
+//  if (!$conn) {
+//      die("Connection failed: " . mysqli_connect_error());// hàm die là hàm chương trình 
+//  }
  
-    // select ALL
-    function selectALL($conn,$table,$select=['*'])
-    {
-        $arr=[];
-        $select=implode(",",$select);
-        $sql="select $select from $table ";
-        $query=mysqli_query($conn,$sql);
-            while($row=mysqli_fetch_array($query)){ 
-            array_push($arr,$row);
-        }
-        return $arr;       
-    }
-    $data= selectALL($conn,'taikhoan1');
+//     // select ALL
+//     function selectALL($conn,$table,$select=['*'])
+//     {
+//         $arr=[];
+//         $select=implode(",",$select);
+//         $sql="select $select from $table ";
+//         die($sql);
+//         $query=mysqli_query($conn,$sql);
+//             while($row=mysqli_fetch_array($query)){ 
+//             array_push($arr,$row);
+//         }
+//         return $arr;       
+//     }
+//     $data= selectALL($conn,'taikhoan');
+//     print_r($data);
     
 ?>
